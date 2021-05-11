aaaf6c97d25f9ca64f9a9ee51947e367bdb39b4d
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-05-11 12:07:11 +0600

    Create controlled component

--- src/AddTaskForm.js
+++ src/AddTaskForm.js
@@ -4,24 +4,41 @@ import './AddTaskForm.css'
 class AddTaskForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
     this.addTask = this.addTask.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
   }
 
   addTask(event) {
     event.preventDefault();
-    let newTask = event.target.task.value;
+    let newTask = this.state.value;
     if (!newTask) {
       return;
     }
-    event.target.task.value = "";
     this.props.addTaskItem(newTask);
+    this.setState({
+      value: '',
+    })
   };
 
   render() {
     return (
       <div>
         <form onSubmit={this.addTask}>
-          <input type="text" name="task" placeholder="Add a task..." />
+          <input
+            type="text"
+            placeholder="Add a task..."
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
           <button type="submit">+</button>
         </form>
       </div>
